530afdac480c8c7e800af6af4721a8b7d6007c7b
Alina <krakhmaalina@gmail.com>
2024-05-31 14:39:43 +0300

    counter in cart-item component

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useAppDispatch } from '../../redux/hooks';
 import { removeProductFromCart } from '../../redux/cart-slice';
 import { Item } from '../../ts/types';
@@ -9,6 +10,18 @@ import style from './style.module.scss';
 function CartItem({ data }: Item) {
   const dispatch = useAppDispatch();
 
+  const [value, setValue] = useState<number>(1);
+
+  const increase = () => {
+    setValue((prev) => prev + 1);
+  };
+
+  const decrease = () => {
+    if (value === 1) return;
+
+    setValue((prev) => prev - 1);
+  };
+
   return (
     <div className={style.cartItem}>
       <img src={data.detail.logoUrl} alt="Sneakers" />
@@ -17,17 +30,28 @@ function CartItem({ data }: Item) {
         <p>Details</p>
       </div>
       <div className={style.counter}>
-        <input type="number" min="1" />
+        <input
+          type="number"
+          min={value}
+          value={value}
+          onChange={(event) => {
+            if (Number(event.target.value)) {
+              setValue(Number(event.target.value));
+            } else {
+              setValue(1);
+            }
+          }}
+        />
         <div className={style.countControls}>
-          <button type="button">
+          <button type="button" onClick={increase}>
             <img src={upIcon} alt="Increase" />
           </button>
-          <button type="button">
+          <button type="button" onClick={decrease}>
             <img src={downIcon} alt="Decrease" />
           </button>
         </div>
       </div>
-      <p>${data.price.item.price / 100}</p>
+      <p>${(data.price.item.price / 100) * value}</p>
       <button
         type="button"
         className={style.deleteButton}
