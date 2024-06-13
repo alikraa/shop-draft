2839053905e5e474c2b5a8bcf4b6d69cd7f6e1c3
Alina <krakhmaalina@gmail.com>
2024-06-13 18:06:17 +0300

    saving brand name from filters

--- src/components/filters/Filters.tsx
+++ src/components/filters/Filters.tsx
@@ -1,24 +1,29 @@
 // import PriceRange from '../price-range/PriceRange';
+import { useAppDispatch } from '../../redux/hooks';
+import { setCurrentBrand } from '../../redux/shop-slice';
 import { brands } from '../../ts/view';
 import style from './style.module.scss';
 
 function Filters() {
+  const dispatch = useAppDispatch();
+
   return (
     <div className={style.filters}>
       <div className={style.brands}>
         <h2>Бренды</h2>
         {brands.map((item, index) => (
-          <label
-            key={index}
-            htmlFor={String(index)}
-          >
+          <label key={index} htmlFor={String(index)}>
             <>
               <input
                 type="checkbox"
                 id={String(index)}
                 value={item}
                 className={style.realCheckbox}
-                onChange={(event) => console.log(event.target.value)}
+                onChange={(event) =>
+                  event.target.checked
+                    ? dispatch(setCurrentBrand(event.target.value))
+                    : dispatch(setCurrentBrand(''))
+                }
               />
               <span className={style.customCheckbox} />
               {item}
--- src/redux/shop-slice.ts
+++ src/redux/shop-slice.ts
@@ -2,10 +2,12 @@ import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 interface InitialState {
   searchValue: string;
+  currentBrand: string;
 }
 
 const initialState: InitialState = {
   searchValue: '',
+  currentBrand: '',
 };
 
 const shopSlice = createSlice({
@@ -15,9 +17,13 @@ const shopSlice = createSlice({
     setSearchValue(state, action: PayloadAction<string>) {
       state.searchValue = action.payload;
     },
+
+    setCurrentBrand(state, action: PayloadAction<string>) {
+      state.currentBrand = action.payload;
+    },
   },
 });
 
-export const { setSearchValue } = shopSlice.actions;
+export const { setSearchValue, setCurrentBrand } = shopSlice.actions;
 
 export default shopSlice.reducer;
